f644b923b0e4a4550ad2df95bbf2e1aff44fc572
George Dilthey <george.dilthey@gmail.com>
2021-09-06 19:52:35 -0400

    Updated fill logic

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-let word = document.querySelector('h1')
+let words = [...document.querySelectorAll('h1')]
 let spans = [...document.querySelectorAll('span')]
 
 function handleType(letter){
-    let result = spans.find(span => span.innerText.toLocaleLowerCase() == letter.toLowerCase() && span.style.color == "")
+    for(word of words){
+        let result = [...word.querySelectorAll('span')].find(span => span.innerText.toLocaleLowerCase() == letter.toLowerCase() && span.style.color == "")
     
-    result ? fill(result) : reset()
+        validateSpan(result) ? fill(result) : resetWord(word)
+    }
+}
+
+let validateSpan = (e) => {
+    if(e){
+        if(e.style.color == ""){
+            if(e.previousElementSibling == null || e.previousElementSibling.style.color == "blue"){
+                return true
+            }
+        }
+    } 
 }
 
 let fill = (e) => {
-    if(e.previousElementSibling == null || e.previousElementSibling.style.color == "blue"){
-        e.style.color = "blue"
-    }          
+    e.style.color = "blue"         
 }
 
-let reset = () => {
-    for(const span of spans){
+let resetWord = (e) => {
+    for(const span of [...e.querySelectorAll('span')]){
         span.style.color = ""
     }
 }
