0113eb69cb4a0415dee1004698a903901e9c9813
George Dilthey <george.dilthey@gmail.com>
2021-09-06 22:46:45 -0400

    added instructions

--- src/index.js
+++ src/index.js
@@ -5,6 +5,23 @@ const start = new Word('Start', 'h1', 'main', log).render()
 const accountWord = new Word('Account', 'h2', 'nav', account).render()
 const scoresWord = new Word('Scores', 'h2', 'nav', account).render()
 
+
+//temp functions
+function log(){
+    console.log("This means the word completed!")
+}
+
+function account(){
+    console.log("This will render the account stuff")
+}
+
+function scores(){
+    console.log("This will render the scores.")
+}
+
+//eventListener
+document.addEventListener('keydown', (e) => handleType(e.key))
+
 function handleType(key){
     for(word of Word.all){
         let letter = word.letters.find(l => l.html.innerText.toLowerCase() == key.toLowerCase() && l.color == "")
@@ -18,17 +35,3 @@ function handleType(key){
         }
     }
 }
-
-function log(){
-    console.log("This means the word completed!")
-}
-
-function account(){
-    console.log("This will render the account stuff")
-}
-
-function scores(){
-    console.log("This will render the scores.")
-}
-
-document.addEventListener('keydown', (e) => handleType(e.key))
